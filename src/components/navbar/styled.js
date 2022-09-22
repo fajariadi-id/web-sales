2419146424fb33c2e83cc6e6b5d7ce80e60e90e0
fajariadi-id <fajariadi.id@gmail.com>
2022-09-22 16:25:50 +0700

    create: navbar

---
+++ src/components/navbar/styled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const NavigationBar = styled.nav`
+  background: ${({ showNav }) =>
+    showNav
+      ? "linear-gradient(to bottom, #222222cc, #33333300);"
+      : "transparent"};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  backdrop-filter: ${({ showNav }) => (showNav ? "blur(5px)" : null)};
+  height: ${({ showNav }) => (showNav ? "80px" : "120px")};
+  box-shadow: ${({ showNav }) => (showNav ? "0 7px 7px rgba(0,0,0,.2)" : null)};
+  transition: all 0.5s ease;
+
+  & img {
+    height: 50px;
+  }
+
+  & .links {
+    gap: 100px;
+  }
+
+  & a {
+    font-family: "Poppins", sans-serif;
+    font-size: 18px;
+    font-weight: 700;
+    color: #f6f6f4 !important;
+    text-shadow: 0 2px #2222224d;
+    cursor: pointer;
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: #3975bb !important;
+      text-shadow: 0 0 #2222224d;
+      font-weight: 700;
+    }
+  }
+`;
