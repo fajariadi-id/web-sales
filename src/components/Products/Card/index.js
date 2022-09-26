da9baa9b81a0752f411905923353237f9e4ff922
fajariadi-id <fajariadi.id@gmail.com>
2022-09-26 11:11:58 +0700

    update: layout product

--- src/components/Products/Card/index.js
+++ src/components/Products/Card/index.js
@@ -1,11 +1,39 @@
 import React from "react";
-import { ArticleCard } from "./styled";
+import { ArticleCard, Triangle } from "./styled";
 import productXenia from "../../../assets/images/cars/productXenia.png";
+import { Button } from "react-bootstrap";
 
 const ProductCard = () => {
   return (
     <ArticleCard className="d-flex justify-content-center align-items-center">
       <img src={productXenia} alt="Xenia" />
+      {/* <Triangle></Triangle> */}
+      <div className="product-label">
+        <span>All New Xenia</span>
+        <div className="triangle"></div>
+      </div>
+
+      <div className="btn-container d-flex justify-content-center">
+        <a
+          href="https://api.whatsapp.com/send?phone=6285156493801&text=Halo, saya mau tau informasi mengenai promo ini dong!"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button className="btn btn-whatsapp d-flex align-items-center">
+            <img
+              className="whatsapp"
+              src="/assets/images/whatsapp.svg"
+              alt="whatsapp"
+            />
+            Whatsapp
+          </Button>
+        </a>
+
+        <Button className="btn btn-detail d-flex align-items-center">
+          <img className="info" src="/assets/images/info.svg" alt="info" />
+          Informasi
+        </Button>
+      </div>
     </ArticleCard>
   );
 };
--- src/components/Products/Card/styled.js
+++ src/components/Products/Card/styled.js
@@ -15,4 +15,74 @@ export const ArticleCard = styled.article`
   &:hover img {
     transform: scale(1.1);
   }
+
+  & .product-label {
+    background-color: red;
+    position: absolute;
+    top: 20px;
+    left: 0;
+    padding: 3px 50px 3px 20px;
+
+    & span {
+      font-family: "Poppins", sans-serif;
+      font-weight: bold;
+      color: #f6f6f4;
+    }
+
+    & .triangle {
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 15px 15px 15px 0;
+      border-color: transparent #f6f6f4 transparent transparent;
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
+
+  & .btn-container {
+    background: linear-gradient(to top, #22222280, #33333300);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 20px;
+    transform: translateY(100%);
+    transition: all 0.5s ease;
+    gap: 10px;
+
+    & .btn-whatsapp {
+      background-color: #00a884;
+      border: none;
+    }
+
+    & .btn-detail {
+      background-color: #0dcaf0;
+      border: none;
+    }
+
+    & .btn {
+      gap: 5px;
+      font-family: "Poppins", sans-serif;
+      font-size: 12px;
+      border-radius: 10px;
+      flex: 1;
+      text-shadow: 0 2px #2222224d;
+    }
+
+    & .whatsapp {
+      width: 20px;
+      height: 20px;
+    }
+
+    & .info {
+      width: 16px;
+      height: 16px;
+    }
+  }
+
+  &:hover .btn-container {
+    transform: translateY(0);
+  }
 `;
